8426aa1e93184cd0d72d77a6f4e5db32b42162dc
goggxi <goggxi@gmail.com>
2024-08-30 16:17:22 +0700

    feat: implemented recipe image api

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -35,7 +35,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = [
-            'id', 'title', 'time_minutes', 'price', 'link', 'tags', 'ingredients',
+            'id', 'title', 'time_minutes', 'price', 'link', 'tags', 'ingredients', 'image',
         ]
         read_only_fields = ['id']
 
@@ -93,3 +93,13 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to recipes."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': 'True'}}
